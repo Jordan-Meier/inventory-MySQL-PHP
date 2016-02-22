b59a8549687520fd9480e9518c4c1e3c99b3d0d0
Jordan Meier and matt knutson <mknut001@gmail.com>
2016-02-22 14:48:39 -0800

    add initial project files and folders

--- app/app.php
+++ app/app.php
@@ -1,6 +1,7 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/ClassName.php";
+    require_once __DIR__."/../src/Inventory.php";
+    require_once __DIR__."/../src/Category.php";
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
---
+++ tests/InventoryTest.php
@@ -0,0 +1,16 @@
+<?php
+
+    require_once "src/Inventory.php";
+
+    class InventoryTest extends PHPUnit_Framework_TestCase
+    {
+
+        function test_method_breakitdown()
+        {
+
+        }
+
+
+    }
+
+?>
